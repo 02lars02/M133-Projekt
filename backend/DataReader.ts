89c9121e578da57611819dc415d69f93e1e02a5a
Lars Ghezzi <lars_ghezzi@sluz.ch>
2019-12-26 19:42:17 +0100

    extend backend to find specified product

--- backend/DataReader.ts
+++ backend/DataReader.ts
@@ -2,7 +2,17 @@ import * as fs from 'fs';
 import { Product } from './Product';
 
 export class DataReader {
-    static readAllProducts() : Product[] {
+    static readAllProducts(): Product[] {
         return JSON.parse(fs.readFileSync('data/products.json', 'utf8')) as Product[];
+    }
+
+    static getProductById(id): Product {
+        let result: Product;
+        DataReader.readAllProducts().forEach(product => {
+            if(product.id == id) {
+                result = product;
+            }
+        });
+        return result;
     }
 }
--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import { DataReader } from './DataReader';
 import * as fs from 'fs';
+import { Product } from "./Product";
 
 const app = express();
 const port = 8080;
@@ -19,6 +20,16 @@ app.get('/api/img/:name', (req, res) => {
             res.send(data);
         }
     });
+});
+
+app.get('/api/product/:id', (req, res) => {
+    let searchedProduct: Product = DataReader.getProductById(req.params.id);
+    if(searchedProduct) {
+        res.header({'Content-Type': 'application/json', 'status': 200});
+        res.send(searchedProduct);
+    } else {
+        res.sendStatus(404);
+    }
 });
 
 app.listen(port, () => console.log(`server started and is listening on port ${port}`));
